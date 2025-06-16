587d4abca86b827fd3756fc2ebc2f1704a0cfaa4
Edri <117730224+EdrianaJoy@users.noreply.github.com>
2025-06-16 22:22:53 +0800

    back button

--- Scripts/Main_Menu.gd
+++ Scripts/Main_Menu.gd
@@ -2,3 +2,7 @@ extends Node2D
 
 func _on_play_pressed() -> void:
 	pass # Replace with function body.
+
+
+func _on_learn_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/GameMech.tscn")
